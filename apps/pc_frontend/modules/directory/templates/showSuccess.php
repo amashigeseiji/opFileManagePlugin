60b8d9d45b027d66817f7b4264c5ec014e937814
amashigeseiji <amashige@tejimaya.com>
2014-06-19 00:41:31 +0900

    directory/showアクションのテンプレート修正

--- apps/pc_frontend/modules/directory/templates/showSuccess.php
+++ apps/pc_frontend/modules/directory/templates/showSuccess.php
@@ -1,11 +1,12 @@
 <h2><?php echo $directory->getName() ?></h2>
-<h3>ファイル</h3>
+<?php echo link_to('設定', 'directory/edit?id='.$directory->id) ?>
 <ul>
 <?php foreach ($files as $file): ?>
   <li>
-  <a href="<?php echo url_for('file_download', $file) ?>" class="btn-small"><i class="icon-arrow-down"></i></a>
-  <?php echo link_to($file->getFileNameWithExtension(), 'file_show', $file) ?>
-  &nbsp;
+    <a href="<?php echo url_for('file_download', $file) ?>" class="btn-small btn-default">
+      <i class="icon-download-alt"></i>
+    </a>
+    <?php echo link_to($file->getFileNameWithExtension(), 'file_show', $file) ?>
   </li>
 <?php endforeach; ?>
 </ul>
