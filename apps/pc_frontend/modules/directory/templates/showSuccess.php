3021ebc7bb49d7611fd0d94b6e8790663965855f
amashigeseiji <amashige@tejimaya.com>
2014-06-20 01:50:09 +0900

    ディレクトリ名の編集リンクを追加

--- apps/pc_frontend/modules/directory/templates/showSuccess.php
+++ apps/pc_frontend/modules/directory/templates/showSuccess.php
@@ -1,3 +1,32 @@
+<script type="text/javascript">
+$(document).ready(function() {
+  $('#directory_edit_name_show_link').on('click', function() {
+    var
+      _self = $(this),
+      to_open = '名前を変更する',
+      to_close = '中止',
+      link = $('.directory_edit_name_form'),
+      href = link.attr('href'),
+      div = $('#directory_edit_name');
+
+    $('.directory_edit_name_input').on('keyup', function () {
+      link.attr('href', href + '?name=' + this.value);
+    });
+
+    if (!div.hasClass('active')) {
+      div.addClass('active');
+      div.show();
+      _self.html(to_close);
+    } else {
+      div.removeClass('active');
+      div.hide();
+      _self.html(to_open);
+    }
+  });
+}
+);
+</script>
+
 <h3>
   <?php echo $directory->getName() ?>
   <?php if ($directory->isAuthor()): ?>
@@ -10,6 +39,12 @@
   <?php else: ?>
     <?php echo link_to('非公開', '@directory_publish?id='.$directory->getId().'&private=true', array('method' => 'put')) ?>
   <?php endif; ?>
+
+  <a href="javascript:void(0)" id="directory_edit_name_show_link">名前を変更する</a>
+  <div id="directory_edit_name" class="hide">
+    <input type="text" class="directory_edit_name_input" />
+    <?php echo link_to('確定', '@directory_edit_name?id='.$directory->getId(), array('method' => 'put', 'class' => 'directory_edit_name_form btn btn-default')) ?>
+  </div>
 <?php endif; ?>
 <ul>
 <?php foreach ($files as $file): ?>
