23dbd774e76fd29eef9a102be528570c1e758964
amashigeseiji <amashige@tejimaya.com>
2014-06-21 12:38:03 +0900

    directory/showのテンプレートを修正

--- apps/pc_frontend/modules/directory/templates/showSuccess.php
+++ apps/pc_frontend/modules/directory/templates/showSuccess.php
@@ -7,10 +7,11 @@
 <?php if ($directory->isAuthor()): ?>
   <?php include_partial('directory/edit', array('directory' => $directory, 'fileForm' => $fileForm)) ?>
 <?php endif; ?>
+<h4>ファイル一覧</h4>
 <ul>
 <?php foreach ($files as $file): ?>
   <li>
-    <a href="<?php echo url_for('file_download', $file) ?>" class="btn-small btn-default">
+    <a href="<?php echo url_for('file_download', $file) ?>" class="btn btn-small">
       <i class="icon-download-alt"></i>
     </a>
     <?php echo link_to($file->getFileNameWithExtension(), 'file_show', $file) ?>
