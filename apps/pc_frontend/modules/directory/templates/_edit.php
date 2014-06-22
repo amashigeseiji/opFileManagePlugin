57f31cf3a6188d6f6eea97b5f204c4c4e2cfe3cb
amashige <amashige@tejimaya.com>
2014-06-23 08:44:50 +0900

    操作部分をボタングループに変更

--- apps/pc_frontend/modules/directory/templates/_edit.php
+++ apps/pc_frontend/modules/directory/templates/_edit.php
@@ -26,23 +26,23 @@ $(document).ready(function() {
   });
 });
 </script>
-<ul class="inline">
-  <li>
-    <?php if (!$directory->getIsOpen()): ?>
-      <?php echo link_to('公開', '@directory_publish?id='.$directory->getId(), array('method' => 'put')) ?>
-    <?php else: ?>
-      <?php echo link_to('非公開', '@directory_publish?id='.$directory->getId().'&private=true', array('method' => 'put')) ?>
-    <?php endif; ?>
-  </li>
 
-  <li>
-    <?php echo link_to('削除', '@directory_delete?id='.$directory->getId(), array('method' => 'delete', 'confirm' => 'フォルダの中のファイルもすべて削除されます。\nよろしいですか？')) ?>
-  </li>
-
-  <li>
-    <a href="javascript:void(0)" id="directory_edit_name_show_link">名前を変更する</a>
-  </li>
-</ul>
+<style>
+<!--
+.btn-group a {
+  color: #333333
+}
+-->
+</style>
+<div class="btn-group">
+  <?php if (!$directory->getIsOpen()): ?>
+    <?php echo link_to('公開', '@directory_publish?id='.$directory->getId(), array('method' => 'put', 'class' => 'btn btn-mini')) ?>
+  <?php else: ?>
+    <?php echo link_to('非公開', '@directory_publish?id='.$directory->getId().'&private=true', array('method' => 'put', 'class' => 'btn btn-mini')) ?>
+  <?php endif; ?>
+  <?php echo link_to('削除', '@directory_delete?id='.$directory->getId(), array('method' => 'delete', 'confirm' => 'フォルダの中のファイルもすべて削除されます。\nよろしいですか？', 'class' => 'btn btn-mini')) ?>
+  <a href="javascript:void(0)" id="directory_edit_name_show_link" class="btn btn-mini">名前を変更する</a>
+</div>
 
 <div id="directory_edit_name" class="hide">
   <input type="text" placeholder="フォルダ名を入力してください" />
