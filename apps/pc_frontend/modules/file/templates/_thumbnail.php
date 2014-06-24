c9e795ec7d4c6aa0762c8943ab78abfe8ea33975
amashigeseiji <amashige@tejimaya.com>
2014-06-25 04:16:27 +0900

    文言変更されていなかったのを修正

--- apps/pc_frontend/modules/file/templates/_thumbnail.php
+++ apps/pc_frontend/modules/file/templates/_thumbnail.php
@@ -6,31 +6,29 @@
 
 <script type="text/javascript">
 $(document).ready( function() {
-  var toggle = function(dom, word) {
-    if (dom.hasClass('active')) {
-      dom.removeClass('active');
+  var toggle = function(dom, link, word) {
+    if (!dom.hasClass('hide')) {
       dom.addClass('hide');
-      $(this).html(word.to_open);
+      link.html(word.to_open);
     } else {
-      dom.addClass('active');
       dom.removeClass('hide');
-      $(this).html(word.to_open);
+      link.html(word.to_close);
     }
   }
 
   $('.toggle-text').on('click', function () {
-    toggle($('.text'), { to_open: 'テキストを表示する', to_close: 'テキストを隠す' });
+    toggle($('.text'), $(this), { to_open: 'テキストを表示する', to_close: 'テキストを隠す' });
   });
 
   $('.toggle-image').on('click', function() {
-    toggle($('.preview-image'), { to_open: '画像を表示する', to_close: '画像を隠す' });
+    toggle($('.preview-image'), $(this), { to_open: '画像を表示する', to_close: '画像を隠す' });
   });
 });
 </script>
 
 <?php if ($file->isImage()): ?>
   <a href="javascript:void(0)" class="toggle-image">画像を表示する</a>
-  <?php echo op_image_tag_sf_image($file->getFile()->getName(), array('size' => '240x320', 'class' => 'hide preview-image')) ?>
+  <?php echo op_image_tag_sf_image($file->getFile()->getName(), array('size' => '320x320', 'class' => 'hide preview-image')) ?>
 <?php elseif ($file->isText()): ?>
 <a href="javascript:void(0)" class="toggle-text">テキストを表示する</a>
 <div class="text hide">
