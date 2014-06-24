553da57513cd555ad1e2d89898a84d6ca92327a8
amashige <amashige@tejimaya.com>
2014-06-24 12:57:03 +0900

    インデントを修正
    インデントの修正および長すぎる行を改行

--- apps/pc_frontend/modules/directory/templates/showSuccess.php
+++ apps/pc_frontend/modules/directory/templates/showSuccess.php
@@ -1,11 +1,11 @@
 <h3>
   <?php echo $directory->getName() ?>
   <?php if ($directory->isAuthor()): ?>
-  <small>(<?php echo $directory->getPublicLabel() ?>)</small>
+    <small>(<?php echo $directory->getPublicLabel() ?>)</small>
   <?php endif; ?>
   <?php if ($directory->isAuthor()): ?>
     <small>
-    <?php include_partial('directory/edit', array('directory' => $directory)) ?>
+      <?php include_partial('directory/edit', array('directory' => $directory)) ?>
     </small>
   <?php endif; ?>
 </h3>
@@ -14,10 +14,19 @@
 <h4>
 ファイル一覧
 <?php if ($directory->getisOpen() || $directory->isAuthor()): ?>
-  <small><a href="javascript:void(0)" id="file_upload_show_link" class="btn btn-mini btn-info" style="color: #ffffff"><?php echo __('Upload') ?></a></small>
-  <?php include_partial('file/fileUploadModal', array('form' => $fileForm, 'url' => url_for('file_upload', $directory), 'trigger' => '#file_upload_show_link')) ?>
+  <small>
+    <a href="javascript:void(0)" id="file_upload_show_link" class="btn btn-mini btn-info" style="color: #ffffff">
+      <?php echo __('Upload') ?>
+    </a>
+  </small>
+  <?php include_partial('file/fileUploadModal', array(
+    'form' => $fileForm,
+    'url' => url_for('file_upload', $directory),
+    'trigger' => '#file_upload_show_link')
+  ) ?>
 <?php endif; ?>
 </h4>
+
 <ul>
 <?php foreach ($files as $file): ?>
   <li>
@@ -29,7 +38,10 @@
         <?php echo link_to(
           '<i class="icon-trash"></i>',
           '@file_delete?id='.$file->getId(),
-          array('method' => 'delete', 'class' => 'btn btn-small', 'confirm' => 'ファイル名: '.$file->getName().'\n本当に削除してもよろしいですか？')) ?>
+          array('method' => 'delete',
+                'class' => 'btn btn-small',
+                'confirm' => 'ファイル名: '.$file->getName().'\n本当に削除してもよろしいですか？')
+        ) ?>
       <?php endif; ?>
     </div>
     <?php echo link_to($file->getName(), 'file_show', $file) ?>
