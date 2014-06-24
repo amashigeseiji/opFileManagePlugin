9a07056d4b2d848f5a853e884a5447cebf4b0ded
amashigeseiji <amashige@tejimaya.com>
2014-06-24 10:10:37 +0900

    編集用ボタンを連結してみた

--- apps/pc_frontend/modules/directory/templates/showSuccess.php
+++ apps/pc_frontend/modules/directory/templates/showSuccess.php
@@ -3,17 +3,21 @@
   <?php if ($directory->isAuthor()): ?>
   <small>(<?php echo $directory->getPublicLabel() ?>)</small>
   <?php endif; ?>
+  <?php if ($directory->isAuthor()): ?>
+    <small>
+    <?php include_partial('directory/edit', array('directory' => $directory)) ?>
+    </small>
+  <?php endif; ?>
 </h3>
 
-<?php if ($directory->isAuthor()): ?>
-  <?php include_partial('directory/edit', array('directory' => $directory)) ?>
-<?php endif; ?>
 
-<h4>ファイル一覧</h4>
+<h4>
+ファイル一覧
 <?php if ($directory->getisOpen() || $directory->isAuthor()): ?>
-  <a href="javascript:void(0)" id="file_upload_show_link"><?php echo __('Upload') ?></a>
+  <small><a href="javascript:void(0)" id="file_upload_show_link" class="btn btn-mini btn-info" style="color: #ffffff"><?php echo __('Upload') ?></a></small>
   <?php include_partial('file/fileUploadModal', array('form' => $fileForm, 'url' => url_for('file_upload', $directory), 'trigger' => '#file_upload_show_link')) ?>
 <?php endif; ?>
+</h4>
 <ul>
 <?php foreach ($files as $file): ?>
   <li>
