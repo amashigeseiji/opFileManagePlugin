269e8dd2b502c2efba30da2616977c8491c6c391
amashigeseiji <amashige@tejimaya.com>
2014-06-28 16:51:30 +0900

    文言の変更

--- apps/pc_frontend/modules/directory/templates/_edit.php
+++ apps/pc_frontend/modules/directory/templates/_edit.php
@@ -36,11 +36,11 @@ $(document).ready(function() {
 </style>
 <div class="btn-group">
   <?php if (!$directory->getIsOpen()): ?>
-    <?php echo link_to('公開', '@directory_publish?id='.$directory->getId(), array('method' => 'put', 'class' => 'btn btn-mini')) ?>
+    <?php echo link_to('公開する', '@directory_publish?id='.$directory->getId(), array('method' => 'put', 'class' => 'btn btn-mini')) ?>
   <?php else: ?>
-    <?php echo link_to('非公開', '@directory_publish?id='.$directory->getId().'&private=true', array('method' => 'put', 'class' => 'btn btn-mini')) ?>
+    <?php echo link_to('非公開にする', '@directory_publish?id='.$directory->getId().'&private=true', array('method' => 'put', 'class' => 'btn btn-mini')) ?>
   <?php endif; ?>
-  <?php echo link_to('削除', '@directory_delete?id='.$directory->getId(), array('method' => 'delete', 'confirm' => 'フォルダの中のファイルもすべて削除されます。\nよろしいですか？', 'class' => 'btn btn-mini')) ?>
+  <?php echo link_to(__('Delete'), '@directory_delete?id='.$directory->getId(), array('method' => 'delete', 'confirm' => 'フォルダの中のファイルもすべて削除されます。\nよろしいですか？', 'class' => 'btn btn-mini')) ?>
   <a href="javascript:void(0)" id="directory_edit_name_show_link" class="btn btn-mini">名前を変更する</a>
 </div>
 
