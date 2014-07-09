e9a699956267e142222ccd609df17b41f8555b37
amashige <amashige@tejimaya.com>
2014-07-09 11:32:07 +0900

    ディレクトリの設定が公開または非公開の場合のみ公開範囲編集ボタンを表示
    現状では公開範囲コミュニティの場合は変更しない

--- apps/pc_frontend/modules/directory/templates/_edit.php
+++ apps/pc_frontend/modules/directory/templates/_edit.php
@@ -8,7 +8,7 @@
 <div class="btn-group">
   <a href="javascript:void(0)" id="directory_edit_name_show_link_<?php echo $directory->getId() ?>" class="btn btn-small"><i class="icon icon-edit"></i></a>
   <?php echo link_to('<i class="icon icon-trash"></i>', '@directory_delete?id='.$directory->getId(), array('method' => 'delete', 'confirm' => 'フォルダ名: '.$directory->getName().'\nフォルダの中のファイルもすべて削除されます。\nよろしいですか？', 'class' => 'btn btn-small')) ?>
-  <?php if (opFileManageConfig::get('use_private_directory')): ?>
+  <?php if ('private' === $directory->getType() || 'public' === $directory->getType()): ?>
     <?php $word = ($directory->isPrivate()) ? '公開する' : '非公開にする' ?>
     <?php $url  = '@directory_publish?id='.$directory->getId().'&redirect='.urlencode($sf_request->getUri()) ?>
     <?php $url .= ($directory->isPrivate()) ? '&publish=public' : '&publish=private'?>
