fa84c9c37cdcf4f5e8672137c36c6d79283f25d1
amashigeseiji <amashige@tejimaya.com>
2014-07-12 17:02:47 +0900

    フォルダの作成フォームをコンポーネント化

--- apps/pc_frontend/modules/directory/templates/_directoryCreateModal.php
+++ apps/pc_frontend/modules/directory/templates/_directoryCreateModal.php
@@ -1,11 +1,9 @@
 <?php
-$form = new FileDirectoryForm();
-?>
-<?php include_partial('file/formModal', array(
+include_partial('file/formModal', array(
   'form'    => $form,
   'url'     => url_for('directory_create'),
   'title'   => 'フォルダを追加する',
   'widgets' => $form->getRenderWidgetNames(),
   'id'      => 'directory_create_form',
   'trigger' => $trigger
-)) ?>
+));
