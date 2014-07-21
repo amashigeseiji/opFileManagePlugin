c216d50482afa85ed217089538e49a903776ad5d
amashigeseiji <amashige@tejimaya.com>
2014-07-21 21:34:43 +0900

    不要な処理を削除

--- apps/pc_frontend/modules/directory/templates/_listRow.php
+++ apps/pc_frontend/modules/directory/templates/_listRow.php
@@ -9,12 +9,10 @@
 <?php if ($directory->isAuthor() && 'community' !== $directory->type): ?>
 <?php echo __($directory->getPublicLabel()) ?>
 <?php if (opFileManageConfig::isUsePrivate()): ?>
-<?php if ('private' === $directory->getType() || 'public' === $directory->getType()): ?>
 <?php $word = ($directory->isPrivate()) ? __('Publish') : __('Take private') ?>
 <?php $url  = '@directory_publish?id='.$directory->getId().'&redirect='.urlencode($sf_request->getUri()) ?>
 <?php $url .= ($directory->isPrivate()) ? '&publish=public' : '&publish=private'?>
 &nbsp;<small>(<?php echo link_to($word, $url, array('method' => 'put')) ?>)</small>
-<?php endif; ?>
 <?php endif; ?>
 <?php endif; ?>
 </td>
