a692a9010d7ddf732a0d59c288431de13fb659ce
amashigeseiji <amashige@tejimaya.com>
2014-07-22 01:46:53 +0900

    ダウンロードリンクの表示

--- apps/pc_frontend/modules/file/templates/showSuccess.php
+++ apps/pc_frontend/modules/file/templates/showSuccess.php
@@ -40,6 +40,15 @@
   <th><?php echo __('Uploaded at') ?></th>
   <td><?php echo $file->getDateTimeObject('created_at')->format('Y年m月d日') ?></td>
 </tr>
+<tr>
+  <th><?php echo __('Download link') ?></th>
+  <td>
+    <div class="input-append">
+      <input type="text" id="download-link" readonly="readonly" value="<?php echo url_for('@file_download?id='.$file->id, true) ?>" style="background-color: white;cursor:auto" onclick="$(this).select()" />
+      <span class="add-on" onclick="$('#download-link').select()"><i class="icon-list-alt"></i></span>
+    </div>
+  </td>
+</tr>
 </tbody>
 </table>
 
