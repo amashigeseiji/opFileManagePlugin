274af0568a78a9f05723a9ec5a270f45f67cbd18
amashige <tenjuu99@gmail.com>
2014-11-01 18:17:09 +0900

    Refactor move directory (refs #14)

--- apps/pc_frontend/modules/file/templates/_operationButton.php
+++ apps/pc_frontend/modules/file/templates/_operationButton.php
@@ -1,5 +1,6 @@
 <?php $member = sfContext::getInstance()->getUser()->getMember() ?>
 <span class="btn-group">
+
   <?php if ($file->isViewable($member)): ?>
   <?php echo link_to(
     '<i class="icon-download-alt"></i>',
@@ -7,6 +8,7 @@
     array('class' => 'btn btn-small'))
   ?>
   <?php endif; ?>
+
   <?php if ($file->isDeletable($member)): ?>
     <?php echo link_to(
       '<i class="icon-trash"></i>',
@@ -16,14 +18,25 @@
             'confirm' => __('File name').': '.$file->getName().'\n'.__('Are you sure you want to remove this completely?'))
     ) ?>
   <?php endif; ?>
+
   <?php if ($file->isEditable($member)): ?>
     <a href="javascript:void(0)" id="file_edit_name_link_<?php echo $file->getId() ?>" class="btn btn-small">
       <i class="icon-edit"></i>
     </a>
   <?php endif; ?>
-  <?php include_partial('file/moveDirectory', array('file' => $file)) ?>
+
+  <?php $movable = $file->isMovable($member) ?>
+  <?php if ($movable): ?>
+    <a href="javascript:void(0)" id="file_move_directory_link_<?php echo $file->id ?>" class="btn btn-small">
+      <i class="icon-folder-open"></i>
+    </a>
+  <?php endif; ?>
+
 </span>
 
 <?php if ($file->isEditable($member)): ?>
-<?php include_partial('file/editFileNameBox', array('file' => $file, 'trigger' => '#file_edit_name_link_'.$file->getId())) ?>
+  <?php include_partial('file/editFileNameBox', array('file' => $file, 'trigger' => '#file_edit_name_link_'.$file->getId())) ?>
+<?php endif; ?>
+<?php if ($movable): ?>
+  <?php include_partial('file/moveDirectory', array('file' => $file, 'trigger' => '#file_move_directory_link_'.$file->id)) ?>
 <?php endif; ?>
