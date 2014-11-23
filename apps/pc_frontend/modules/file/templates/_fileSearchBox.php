978930b1ac7b0b8f4edbc435b7c3dd11c165bb04
amashige <tenjuu99@gmail.com>
2014-11-24 04:40:09 +0900

    modify placeholder (refs #23)

--- apps/pc_frontend/modules/file/templates/_fileSearchBox.php
+++ apps/pc_frontend/modules/file/templates/_fileSearchBox.php
@@ -2,8 +2,8 @@
   <form action="<?php echo url_for('@file_index') ?>" method="get" class="form-inline">
     <div class="form-group">
     <?php $params = $sf_request->getParameter('file') ?>
-      <input type="text" placeholder="ファイル名" name="file[name]" value="<?php echo $params['name'] ?>" />
-      <input type="text" placeholder="説明" name="file[note]" value="<?php echo $params['note'] ?>" />
+      <input type="text" placeholder="<?php echo __('File name') ?>" name="file[name]" value="<?php echo $params['name'] ?>" />
+      <input type="text" placeholder="<?php echo __('note') ?>" name="file[note]" value="<?php echo $params['note'] ?>" />
       <input type="hidden" name="search" value="true">
       <button type="submit" class="btn btn-small">検索</button>
     </div>
