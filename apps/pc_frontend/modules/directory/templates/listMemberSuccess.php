1835b307e5c6a3e13929465638215280a8355a68
amashige <tenjuu99@gmail.com>
2014-11-29 18:47:59 +0900

    Fix directory pager url is wrong (refs #34)

--- apps/pc_frontend/modules/directory/templates/listMemberSuccess.php
+++ apps/pc_frontend/modules/directory/templates/listMemberSuccess.php
@@ -6,7 +6,7 @@
 
 <?php include_partial('directory/listTable', array('pager' => $pager)) ?>
 
-<?php op_include_pager_navigation($pager, '@directory_list?id='.$member->getId().'&page=%d'); ?>
+<?php op_include_pager_navigation($pager, '@directory_list_member?id='.$member->getId().'&page=%d'); ?>
 <?php else: ?>
 <?php op_include_box('DirectoryList', __('There is no directory.')) ?>
 <?php endif; ?>
